78d83c0eedb1702a59b69b6519bf411c00964f1b
adaFetic <adafetic@gmail.com>
2023-06-22 21:48:38 -0500

    altered d-c

--- src/test/java/com/cydeo/runner/CukesRunner.java
+++ src/test/java/com/cydeo/runner/CukesRunner.java
@@ -13,6 +13,7 @@ import org.junit.runner.RunWith;
                 "rerun:target/rerun.txt"},
         features = "src/test/resources/features",
         glue = "com/cydeo/step_definitions",
+        publish = true,
         dryRun = false,
         tags = ""
 )
